b94226b5509de2786d476525ad29b45dbd048584
ignacio <ignaciomenchaca666@gmail.com>
2019-08-27 00:08:44 -0500

    Initial commit

---
+++ web/src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import ListBook from './views/book/List'
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/books',
+      name: 'ListBook',
+      component: ListBook
+    },
+  ]
+})
---
+++ web/src/views/book/List.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col text-left">
+        <h2>List of books</h2>
+        <div class="col-md-12">
+          <b-table striped hover :items="books.data" :fields="fields">
+            <template slot="action" slot-scope="data">
+              <b-button pill variant="primary">Edit</b-button>
+              <b-button pill variant="danger">Delete</b-button>
+            </template>
+          </b-table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ListBook",
+        data() {
+        	return {
+                books: [],
+                fields: [
+                    {key: 'title', label: 'Título'},
+                    {key: 'description', label: 'Descripción'},
+                    {key: 'action', label: ''},
+                ]
+            }
+        },
+        created(){
+        	this.getBooks();
+        },
+        methods: {
+        	async getBooks() {
+        		try {
+					this.books = await this.axios.get(`${this.$api_url}book`);
+				} catch (e) {
+        			console.log(e);
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
